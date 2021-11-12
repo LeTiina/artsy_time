866bc364394af6ce8c37d060eb523e0464df84cf
LeTiina <tiina.j.le@gmail.com>
2021-11-12 13:12:37 +0200

    updated code

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import colorgram
 
 colors = colorgram.extract('truniokuva.jpg',6)
-first_color = colors[0]
+print(colors)
+list_of_colors = []
+for i in range(0, 6):
+    color = colors[i]
+    print(color.rgb)
+    list_of_colors.append(color.rgb)
+
+print(list_of_colors)
