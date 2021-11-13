82fe21ff914e5bcb1681e54f4955efcab007a796
LeTiina <tiina.j.le@gmail.com>
2021-11-13 14:59:48 +0200

    updated code / drawing the painting in black and white

--- main.py
+++ main.py
@@ -22,13 +22,23 @@ print(list_of_colors)
 timmy_the_painter = Turtle()
 timmy_the_painter.penup()
 timmy_the_painter.goto(-250,-250)
-i = 0
-while i < 11:
-    timmy_the_painter.pendown()
-    timmy_the_painter.dot(20)
-    timmy_the_painter.penup()
-    timmy_the_painter.forward(50)
-    i+=1
+timmy_the_painter.speed(200)
 
+def draw_line():
+    i = 0
+    while i < 10:
+        timmy_the_painter.pendown()
+        timmy_the_painter.dot(20)
+        timmy_the_painter.penup()
+        timmy_the_painter.forward(50)
+        i+=1
+a = 0
+position = -200
+while a < 10:
+    draw_line()
+    timmy_the_painter.penup()
+    timmy_the_painter.goto(-250,position)
+    a +=1
+    position += 50
 screen = Screen()
 screen.exitonclick()
