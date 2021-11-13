ab759ac5120256b549bab767610cdb83f8fbf744
LeTiina <tiina.j.le@gmail.com>
2021-11-13 12:14:59 +0200

    updated code / list of colors done

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import colorgram
 
 colors = colorgram.extract('truniokuva.jpg',6)
-print(colors)
 list_of_colors = []
 for i in range(0, 6):
     color = colors[i]
-    print(color.rgb)
-    list_of_colors.append(color.rgb)
+    rgb = color.rgb
+    red = rgb[0]
+    blue = rgb[1]
+    yellow = rgb[2]
+    tuple_for_the_list = (red, blue, yellow)
+    list_of_colors.append(tuple_for_the_list)
 
 print(list_of_colors)
