0346a488b45b6d6365af19b964360fad01be8ec1
LeTiina <tiina.j.le@gmail.com>
2021-11-14 19:56:56 +0200

    completed: draws a dot painting from a picture

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import turtle
-
 import colorgram
 from turtle import Turtle,Screen
+import random
 
 colors = colorgram.extract('truniokuva.jpg',20)
 list_of_colors = []
@@ -15,30 +14,35 @@ for i in range(0, 20):
     list_of_colors.append(tuple_for_the_list)
 
 print(list_of_colors)
-#10*10 rows of spots
-#dot 20 in size
-#space between 50
 
 timmy_the_painter = Turtle()
+screen = Screen()
+screen.colormode(255)
 timmy_the_painter.penup()
 timmy_the_painter.goto(-250,-250)
 timmy_the_painter.speed(200)
 
+
 def draw_line():
-    i = 0
-    while i < 10:
+    x = 0
+    while x < 10:
+        random_color = random.randrange(0, 20)
+        print(random_color)
+        timmy_the_painter.pencolor(list_of_colors[random_color])
         timmy_the_painter.pendown()
         timmy_the_painter.dot(20)
         timmy_the_painter.penup()
         timmy_the_painter.forward(50)
-        i+=1
+        x += 1
+
+
 a = 0
 position = -200
 while a < 10:
     draw_line()
     timmy_the_painter.penup()
     timmy_the_painter.goto(-250,position)
-    a +=1
+    a += 1
     position += 50
-screen = Screen()
+
 screen.exitonclick()
